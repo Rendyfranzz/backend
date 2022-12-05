68f89d6627695f1cd90be3c775d0dc888f7887cb
Rendi <rendicrew96@gmail.com>
2022-12-05 17:51:18 +0700

    update

--- controllers/Auth.js
+++ controllers/Auth.js
@@ -11,7 +11,6 @@ export const Login = async(req, res)=>{
     const match = await argon2.verify(user.password, req.body.password);
     if(!match) return res.status(400).json({msg:"password salah"});
     req.session.userId = user.uuid;
-    console.log(req.session.userId);
     const uuid = user.uuid;
     const name = user.name;
     const email = user.email;
