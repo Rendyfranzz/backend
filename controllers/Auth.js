82d341014ace779db7ed44e949dd39d64c18f2a8
Rendi <rendicrew96@gmail.com>
2022-12-05 19:19:25 +0700

    update

--- controllers/Auth.js
+++ controllers/Auth.js
@@ -15,35 +15,35 @@ export const Login = async(req, res)=>{
     const email = user.email;
     const role = user.role;
     req.session.user = user.uuid
-    
-    // res.cookie('cookie',user.uuid,{sameSite:"none",secure:true})
+    res.cookie('cookie',user.uuid,{sameSite:"none",secure:true})
     res.status(200).json({uuid,name,email,role})
-    res.send(req.session.user);
-    
 }
 
 export const LogOut = (req, res)=>{
-    // req.clearCookie('cookie')
-    req.session.destroy((err)=>{
-        if(err) return res.status(400).json({msg:"Tidak dapat Log Out"});
-        res.status(200).json({msg:"Anda telah log out"})
-    })
+    res.writeHead(200, {
+        "Set-Cookie": `cookie=; HttpOnly; path=/; max-age=0`,
+    });
+    res.end();
+    // req.session.destroy((err)=>{
+    //     if(err) return res.status(400).json({msg:"Tidak dapat Log Out"});
+    //     res.status(200).json({msg:"Anda telah log out"})
+    // })
 }
 
 export const Me = async(req, res)=>{
     res.header("Access-Control-Allow-Credentials",true)
     console.log(req.session);
-    // const cookie = req.headers.cookie?.split("=").pop()
-    // console.log(cookie);
-    // if(!cookie){
-    //     return res.status(401).json({msg:"Mohon login ke akun anda"})
-    // }
-    // const user = await Users.findOne({
-    //     attributes:["uuid","name","email","role","hp"],
-    //     where:{
-    //         uuid: cookie
-    //     }
-    // });
-    // if(!user) return res.status(404).json({msg:"user tidak ditemukan"})
-    // res.status(200).json(user)
+    const cookie = req.headers.cookie?.split("=").pop()
+    console.log(cookie);
+    if(!cookie){
+        return res.status(401).json({msg:"Mohon login ke akun anda"})
+    }
+    const user = await Users.findOne({
+        attributes:["uuid","name","email","role","hp"],
+        where:{
+            uuid: cookie
+        }
+    });
+    if(!user) return res.status(404).json({msg:"user tidak ditemukan"})
+    res.status(200).json(user)
 }
