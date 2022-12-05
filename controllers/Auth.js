012be516d69a80694f72c7e3991e09e995cd0d40
Rendi <rendicrew96@gmail.com>
2022-12-05 17:49:19 +0700

    update

--- controllers/Auth.js
+++ controllers/Auth.js
@@ -30,16 +30,16 @@ export const LogOut = (req, res)=>{
 
 export const Me = async(req, res)=>{
     res.header("Access-Control-Allow-Credentials",true)
-    console.log(req.session);
-    if(!req.session.userId){
-        return res.status(401).json({msg:"Mohon login ke akun anda"})
-    }
-    const user = await Users.findOne({
-        attributes:["uuid","name","email","role","hp"],
-        where:{
-            uuid: req.session.userId
-        }
-    });
-    if(!user) return res.status(404).json({msg:"user tidak ditemukan"})
-    res.status(200).json(user)
+    console.log(req.header.cookie);
+    // if(!req.session.userId){
+    //     return res.status(401).json({msg:"Mohon login ke akun anda"})
+    // }
+    // const user = await Users.findOne({
+    //     attributes:["uuid","name","email","role","hp"],
+    //     where:{
+    //         uuid: req.session.userId
+    //     }
+    // });
+    // if(!user) return res.status(404).json({msg:"user tidak ditemukan"})
+    // res.status(200).json(user)
 }
