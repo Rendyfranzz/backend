ec9fb234957a13fa5e981c2485f39b11be9d4949
Rendi <rendicrew96@gmail.com>
2022-12-05 17:35:00 +0700

    update

--- controllers/Auth.js
+++ controllers/Auth.js
@@ -18,7 +18,6 @@ export const Login = async(req, res)=>{
     const role = user.role;
     res.cookie('cookie2',user.uuid,{sameSite:"none",secure:true})
     res.status(200).json({uuid,name,email,role})
-    
 }
 
 export const LogOut = (req, res)=>{
@@ -31,15 +30,15 @@ export const LogOut = (req, res)=>{
 export const Me = async(req, res)=>{
     res.header("Access-Control-Allow-Credentials",true)
     console.log(req.session);
-    if(!req.session.userId){
-        return res.status(401).json({msg:"Mohon login ke akun anda"})
-    }
-    const user = await Users.findOne({
-        attributes:["uuid","name","email","role","hp"],
-        where:{
-            uuid: req.session.userId
-        }
-    });
-    if(!user) return res.status(404).json({msg:"user tidak ditemukan"})
-    res.status(200).json(user)
+    // if(!req.session.userId){
+    //     return res.status(401).json({msg:"Mohon login ke akun anda"})
+    // }
+    // const user = await Users.findOne({
+    //     attributes:["uuid","name","email","role","hp"],
+    //     where:{
+    //         uuid: req.session.userId
+    //     }
+    // });
+    // if(!user) return res.status(404).json({msg:"user tidak ditemukan"})
+    // res.status(200).json(user)
 }
